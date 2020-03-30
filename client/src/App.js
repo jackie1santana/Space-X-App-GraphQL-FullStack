d695b4bc14a317418f72fc0a0d1a6d681cae4caf
Jackie Santana <jackiebrown.nyc@gmail.comgit config --global user.email jackiebrown.nyc@gmail.com>
2020-03-30 12:57:33 -0500

    finished react app

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import './App.css';
 
 // wrap the main component in this & pass in the client
 const client = new ApolloClient({
-  uri: 'http://localhost:5000/graphql'
+  uri: '/graphql'
 })
 
 function App() {
---
+++ public/precache-manifest.97e4a1c5895d480fa078986df97d0084.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53e46438d2ff382481853e0049cd0601",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f4df1f127b0769c8e02",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "1081bcc99d6e3aeba233",
+    "url": "/static/js/2.59bf6197.chunk.js"
+  },
+  {
+    "revision": "f0d43bb23094b147d2c500466b241ef2",
+    "url": "/static/js/2.59bf6197.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f4df1f127b0769c8e02",
+    "url": "/static/js/main.45941bb9.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
